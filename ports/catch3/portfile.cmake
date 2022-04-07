eefde9eb226058753316155a2c7021e6dfe20b63
marcgardent <marcgardent@yahoo.fr>
2022-04-07 18:05:55 +0200

    add catch3   3.0.0-preview.4 hash added

--- ports/catch3/portfile.cmake
+++ ports/catch3/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO catchorg/Catch2
     REF v3.0.0-preview4
-    SHA512 1
+    SHA512 3a879da07dd5f4e2bcd0d5e1be0de5b0128930c3a5a343805b308ddc788618bb0a9d5ea6de673cf8745f7997eebafabb65d29ee39120bf82218f8df0f47be039
     HEAD_REF devel
 )
 
